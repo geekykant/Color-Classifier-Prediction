a98e23cef9e0d10cf038264096e0b025f24eb585
Sreekant <geekykant@gmail.com>
2019-01-13 17:40:35 +0530

    click changes

--- sketch.js
+++ sketch.js
@@ -3,6 +3,9 @@ let r, g, b;
 function setup() {
   createCanvas(200, 200);
 	setupBackgroundColor();
+	buttons = ['red-ish','blue-ish','yellow-ish','purple-ish','white-ish','green-ish'];
+	button = createButton('submit');
+	button.mousePressed(buttonClicked);
 }
 
 function setupBackgroundColor() {
@@ -10,4 +13,8 @@ function setupBackgroundColor() {
   g = floor(random(256));
   b = floor(random(256));
   background(r, g, b);
+}
+
+function buttonClicked(){
+	setupBackgroundColor();
 }
