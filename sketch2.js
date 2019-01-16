13e6fa4e197924022853f1a8606e4142483d5107
Sreekant <geekykant@gmail.com>
2019-01-16 23:18:14 +0530

    optimizer and fitted

--- sketch2.js
+++ sketch2.js
@@ -1,4 +1,5 @@
 let data;
+let model;
 
 function preload() {
   data = loadJSON('colorData.json');
@@ -26,12 +27,44 @@ function setup() {
 
   let xs = tf.tensor2d(colors);
 
-  let labelsTensor = tf.tensor1d(labels,'int32');
-  let ys = tf.oneHot(labelsTensor,9)
+  let labelsTensor = tf.tensor1d(labels, 'int32');
+  let ys = tf.oneHot(labelsTensor, 9)
   labelsTensor.dispose();
 
   xs.print();
   ys.print();
+
+  model = tf.sequential();
+
+  let hidden = tf.layers.dense({
+    units: 16,
+    activation: 'sigmoid',
+    inputDim: 3
+  });
+  let output = tf.layers.dense({
+    units: 9,
+    activation: 'softmax'
+  });
+
+  model.add(hidden);
+  model.add(output);
+
+  //optimizer function
+  const lr = 0.2;
+  const optimizer = tf.train.sgd(lr);
+
+  model.compile({
+    optimizer: optimizer,
+    loss: 'categoricalCrossentropy'
+  });
+
+  const options = {
+    epochs: 3
+  }
+
+  model.fit(xs, ys, options).then(results => {
+    console.log(results.history.loss);
+  });
 
   // console.log(xs.shape);
 }
