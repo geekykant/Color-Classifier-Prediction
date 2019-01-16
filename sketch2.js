aeebca3c952678ee97cc76d84ae865335002e012
Sreekant <geekykant@gmail.com>
2019-01-16 21:10:20 +0530

    labels and entries added

--- sketch2.js
+++ sketch2.js
@@ -4,7 +4,25 @@ function preload() {
   data = loadJSON('colorData.json');
 }
 
+let colorList = [
+  'red-ish',
+  'green-ish',
+  'blue-ish',
+  'orange-ish',
+  'yellow-ish',
+  'pink-ish',
+  'purple-ish',
+  'brown-ish',
+  'grey-ish'
+]
+
 function setup() {
   let colors = [];
-  console.log(data.entries.length);
+  let labels = [];
+  for (let record of data.entries) {
+    colors.push([record.r / 255, record.g / 255, record.b / 255]);
+    labels.push(colorList.indexOf(record.label));
+  }
+
+  console.log(labels);
 }
